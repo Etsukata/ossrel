9f8621160ab75f633d1f71b99059c6151ca13b5e
Eiichi Tsukata <devel@etsukata.com>
2013-11-19 22:41:00 +0900

    implement random coloring

--- color.py
+++ color.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
-""" This script makes color of each project
+""" This script makes color of each project.
 """
 
 import os
+import random
 
 def main():
 
@@ -11,7 +12,11 @@ def main():
     for proj in os.listdir('./ranking/'):
         dir = {}
         dir['proj'] = proj
-        dir['color'] = '#FF7F00'
+        color = '#'
+        crange = '89ABCDE'
+        for i in range(6):
+            color += crange[random.randint(0, 6)]
+        dir['color'] = color
         array.append(dir)
 
     print 'var projects = ' + str(array)
